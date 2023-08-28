069b9ffc2d836514aa34e203f59651ad6b5a2d9e
ankanaich98 <ankan98aich@gmail.com>
2023-08-28 20:19:18 +0600

    Added Login Page

---
+++ Tea-Schedule-Management/src/main/java/com/example/TeaScheduleManagement/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.TeaScheduleManagement.configuration.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin = User.withUsername("ankan").password(encoder.encode("123456")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.loginPage("/login").permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/").permitAll())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/assets/**", "/css/**", "/js/**","/login").permitAll().anyRequest().authenticated())
+//                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/",true).permitAll())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/admin").authenticated())
+//                .logout(logout-> logout.logoutUrl("/sign-out").logoutSuccessUrl("/"))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
